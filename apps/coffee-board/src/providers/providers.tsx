8515acf7080817050e5a55b6159a8600acac02b5
Carlos Estrada <carabesle@gmail.com>
2025-05-04 12:25:27 -0600

    feat: add basic providers for the app

---
+++ apps/coffee-board/src/providers/providers.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react"
+import TanstackProvider from "./tanstack-provider"
+import { Toaster } from "sonner"
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<Toaster richColors expand closeButton position="top-right" />
+			<TanstackProvider>{children}</TanstackProvider>
+		</>
+	)
+}
