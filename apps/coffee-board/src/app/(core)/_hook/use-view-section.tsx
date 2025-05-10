800d1aa85abc28426381d08faff0442e2551dc0a
Carlos Estrada <carabesle@gmail.com>
2025-05-09 19:41:15 -0600

    feat: add use view section hook

---
+++ apps/coffee-board/src/app/(core)/_hook/use-view-section.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from "react"
+
+export const VIEW_SECTION = {
+	OVERVIEW: "OVERVIEW",
+	BOARD: "BOARD",
+	CALENDAR: "CALENDAR",
+}
+
+interface IViewSectionContext {
+	section: (typeof VIEW_SECTION)[keyof typeof VIEW_SECTION]
+	setSection: (
+		section: (typeof VIEW_SECTION)[keyof typeof VIEW_SECTION],
+	) => void
+}
+
+const ViewSectionContext = createContext<IViewSectionContext>({
+	section: VIEW_SECTION.BOARD,
+	setSection: () => {},
+})
+
+export const useViewSection = () => useContext(ViewSectionContext)
+
+export function ViewSectionProvider({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	const [section, setSection] = useState<
+		(typeof VIEW_SECTION)[keyof typeof VIEW_SECTION]
+	>(VIEW_SECTION.BOARD)
+
+	const value: IViewSectionContext = {
+		section,
+		setSection,
+	}
+
+	return <ViewSectionContext value={value}>{children}</ViewSectionContext>
+}
