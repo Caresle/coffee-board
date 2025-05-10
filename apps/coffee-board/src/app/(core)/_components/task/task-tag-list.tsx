84e185e1a78d32f1d357c3bccb932a8c0328c912
Carlos Estrada <carabesle@gmail.com>
2025-05-09 19:22:59 -0600

    feat: add task tag list component

---
+++ apps/coffee-board/src/app/(core)/_components/task/task-tag-list.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function TaskTagList() {
+	return (
+		<div>
+			<div className="bg-red-50 w-fit text-red-500 rounded-sm px-2 py-1 text-xs">
+				High Priority
+			</div>
+		</div>
+	)
+}
