4dc37a4d63bb46023e1f85a5dcc21bad34d6e21b
Carlos Estrada <carabesle@gmail.com>
2025-05-18 15:32:04 -0600

    feat(ui): add priority section Fixes #86

---
+++ apps/coffee-board/src/app/(core)/(sidebar-sections)/priorities/client.tsx
@@ -0,0 +1,42 @@
+"use client"
+import Icons from "@/components/shared/icons"
+import { Button } from "@/components/ui/button"
+import React from "react"
+import PriorityModal from "./_components/_modals/priority-modal"
+import { usePriorityStore } from "./_states/priority.state"
+import PriorityDeleteModal from "./_components/_modals/priority-delete-modal"
+import PriorityItem from "./_components/priority-item"
+
+const CreatePriorityButton = () => {
+	const { update } = usePriorityStore(state => state)
+	return (
+		<div>
+			<Button onClick={() => update({ show: true, isEdit: false })}>
+				<Icons.Actions.Add />
+				Create Priority
+			</Button>
+		</div>
+	)
+}
+
+export default function Client() {
+	return (
+		<div className="flex-1 flex flex-col gap-2 overflow-y-auto p-2">
+			<PriorityModal />
+			<PriorityDeleteModal />
+
+			<h1 className="text-2xl font-semibold">Priorities</h1>
+			<div className="bg-slate-100 p-2 rounded-lg">
+				<CreatePriorityButton />
+			</div>
+
+			<div className="flex-1 rounded-lg p-2 border bg-slate-100 flex flex-col gap-2 overflow-y-auto">
+				{Array(10)
+					.fill(0)
+					.map((_, index) => (
+						<PriorityItem key={index} />
+					))}
+			</div>
+		</div>
+	)
+}
