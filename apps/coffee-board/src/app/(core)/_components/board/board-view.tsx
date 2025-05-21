26cfda1e11583d8a8bb4ae284240045d16c53899
Carlos Estrada <carabesle@gmail.com>
2025-05-20 19:46:03 -0600

    feat(ui): button for add more card containers Fixes #10

--- apps/coffee-board/src/app/(core)/_components/board/board-view.tsx
+++ apps/coffee-board/src/app/(core)/_components/board/board-view.tsx
@@ -1,10 +1,24 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
 import BoardCard from "./board-card"
+import { Button } from "@/components/ui/button"
+import Icons from "@/components/shared/icons"
 
 export default function BoardView() {
+	const [boards, setBoards] = useState([{}])
+
+	const onAddBoard = () => {
+		setBoards([...boards, {}])
+	}
+
 	return (
-		<div className="flex-1 overflow-y-auto grid grid-cols-4 gap-2">
-			<BoardCard />
+		<div className="flex-1 overflow-y-auto flex gap-2 overflow-x-auto">
+			{boards.map((_, index) => (
+				<BoardCard key={index} />
+			))}
+			<Button variant={"secondary"} onClick={onAddBoard} className="w-[300px]">
+				<Icons.Actions.Add />
+			</Button>
 		</div>
 	)
 }
