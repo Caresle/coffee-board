ff60446fa67d0d0733d31caf907ba4502f7c34e4
Carlos Estrada <carabesle@gmail.com>
2025-05-24 09:10:27 -0600

    feat(ui): switch theme Fixes #8

---
+++ apps/coffee-board/src/components/shared/theme/theme-switcher.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import TooltipBasic from "../tooltip-basic"
+import { Button } from "@/components/ui/button"
+import Icons from "../icons"
+import { useTheme } from "next-themes"
+
+export default function ThemeSwitcher() {
+	const { theme, setTheme, resolvedTheme } = useTheme()
+
+	const onToggle = () => {
+		const currentTheme = theme === "system" ? resolvedTheme : theme
+		setTheme(currentTheme === "dark" ? "light" : "dark")
+	}
+
+	return (
+		<TooltipBasic title="Swith Theme">
+			<Button size={"icon"} variant={"secondary"} onClick={onToggle}>
+				{theme === "dark" ? <Icons.Misc.Sun /> : <Icons.Misc.Moon />}
+			</Button>
+		</TooltipBasic>
+	)
+}
