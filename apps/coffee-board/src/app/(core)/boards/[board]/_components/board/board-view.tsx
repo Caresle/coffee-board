979d6b0fddc9450a4978603b1945516f60689360
Carlos Estrada <carabesle@gmail.com>
2025-05-23 20:01:06 -0600

    feat(ui): modal for task creation Fixes #9

--- apps/coffee-board/src/app/(core)/boards/[board]/_components/board/board-view.tsx
+++ apps/coffee-board/src/app/(core)/boards/[board]/_components/board/board-view.tsx
@@ -4,6 +4,7 @@ import BoardCard from "./board-card"
 import { Button } from "@/components/ui/button"
 import Icons from "@/components/shared/icons"
 import DeleteTaskModal from "../_modals/delete-task-modal"
+import TaskModal from "../_modals/task-modal"
 
 export default function BoardView() {
 	const [boards, setBoards] = useState([{}])
@@ -14,6 +15,7 @@ export default function BoardView() {
 
 	return (
 		<>
+			<TaskModal />
 			<DeleteTaskModal />
 
 			<div className="flex-1 overflow-y-auto flex gap-2 overflow-x-auto">
