80f94b02c39f6b29cf11767d55c9eb4ee9670f47
Carlos Estrada <carabesle@gmail.com>
2025-05-24 20:24:48 -0600

    feat(ui-dark): add support for tags Fixes #116

--- apps/coffee-board/src/app/(core)/(sidebar-sections)/tags/_components/tag-section.tsx
+++ apps/coffee-board/src/app/(core)/(sidebar-sections)/tags/_components/tag-section.tsx
@@ -9,9 +9,9 @@ export default function TagSection({
 	title: string
 }) {
 	return (
-		<div className="bg-white rounded-lg p-2 border flex flex-col gap-2 overflow-y-auto">
+		<div className="bg-white rounded-lg p-2 border flex flex-col gap-2 overflow-y-auto dark:bg-slate-800">
 			<h2>{title}</h2>
-			<div className="flex-1 bg-slate-100 overflow-y-auto rounded-lg flex flex-col gap-2 p-2">
+			<div className="flex-1 bg-slate-100 overflow-y-auto rounded-lg flex flex-col gap-2 p-2 dark:bg-slate-900">
 				{tags.map((tag, index) => (
 					<TagItem key={index} tag={tag} />
 				))}
