2bb37c9eb725662001998aef3b0271192b1e5194
Carlos Estrada <carabesle@gmail.com>
2025-05-24 20:36:04 -0600

    feat(ui-dark): add support for notifications Fixes #118

--- apps/coffee-board/src/app/(core)/notifications/client.tsx
+++ apps/coffee-board/src/app/(core)/notifications/client.tsx
@@ -6,7 +6,7 @@ export default function Client() {
 	return (
 		<div className="flex-1 flex flex-col gap-2 overflow-y-auto p-2">
 			<h2 className="text-2xl font-semibold">Notifications</h2>
-			<div className="flex flex-col gap-2 bg-slate-100 flex-1 rounded-lg p-2 overflow-y-auto">
+			<div className="flex flex-col gap-2 bg-slate-100 flex-1 rounded-lg p-2 overflow-y-auto dark:bg-neutral-900">
 				<NotificationItem variant="error" />
 				<NotificationItem variant="warning" />
 				<NotificationItem variant="success" />
