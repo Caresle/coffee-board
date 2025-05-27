2303b768a130303e790cccfdfa31885daede0be7
Carlos Estrada <carabesle@gmail.com>
2025-05-26 19:53:39 -0600

    feat: add priority provider to client page

--- apps/coffee-board/src/actions/priority/get-all-priorities.ts
+++ apps/coffee-board/src/actions/priority/get-all-priorities.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { Priority } from "@/entities/priority.entity"
 import { pgQuery } from "@/lib/pg"
 
 const query = `
@@ -7,10 +8,10 @@ const query = `
     order by value desc
 `
 
-export async function getAllPriorities() {
+export async function getAllPriorities(): Promise<Priority[]> {
 	try {
 		const data = await pgQuery(query)
-		return data
+		return data as Priority[]
 	} catch (error) {
 		console.error(error)
 		return []
--- apps/coffee-board/src/app/(core)/(sidebar-sections)/priorities/page.tsx
+++ apps/coffee-board/src/app/(core)/(sidebar-sections)/priorities/page.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import Client from "./client"
+import { getAllPriorities } from "@/actions/priority/get-all-priorities"
 
-export default function PrioritiesPage() {
-	return <Client />
+export default async function PrioritiesPage() {
+	const data = await getAllPriorities()
+
+	return <Client initialPriorities={data} />
 }
