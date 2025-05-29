cd0b14c3dff5114795da0928fcebc71f1dfd88cd
Carlos Estrada <carabesle@gmail.com>
2025-05-28 19:54:07 -0600

    feat: add custom hook for projects section

--- apps/coffee-board/src/app/(core)/(sidebar-sections)/projects/client.tsx
+++ apps/coffee-board/src/app/(core)/(sidebar-sections)/projects/client.tsx
@@ -1,25 +1,29 @@
 "use client"
 
 import React from "react"
-import ProjectCard from "./_components/project-card"
 import ArchiveModal from "./_components/_modal/archive-modal"
 import DeleteProjectModal from "./_components/_modal/delete-project-modal"
 import TopActions from "./_components/top-actions"
 import ProjectModal from "./_components/_modal/project-modal"
+import { ProjectProvider } from "./_hook/use-projects"
+import { Project } from "@/entities/project.entity"
+import ProjectContainer from "./_components/project-container"
 
-export default function Client() {
+export default function Client({
+	initialProjects = [],
+}: {
+	initialProjects: Project[]
+}) {
 	return (
-		<div className="flex-1 flex flex-col gap-2 overflow-y-auto p-2">
-			<ArchiveModal />
-			<DeleteProjectModal />
-			<ProjectModal />
+		<ProjectProvider initialProjects={initialProjects}>
+			<div className="flex-1 flex flex-col gap-2 overflow-y-auto p-2">
+				<ArchiveModal />
+				<DeleteProjectModal />
+				<ProjectModal />
 
-			<TopActions />
-			<div className="flex-1 overflow-y-auto bg-slate-100 rounded-lg dark:bg-neutral-900">
-				<div className="gap-2 overflow-y-auto p-2 grid grid-cols-3">
-					<ProjectCard />
-				</div>
+				<TopActions />
+				<ProjectContainer />
 			</div>
-		</div>
+		</ProjectProvider>
 	)
 }
