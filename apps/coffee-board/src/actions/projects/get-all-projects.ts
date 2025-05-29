c752f5a385822d7e03ec8b05d4aed6b1a9535401
Carlos Estrada <carabesle@gmail.com>
2025-05-28 19:53:45 -0600

    feat: add get all projects in server before initial loading page

--- apps/coffee-board/src/actions/projects/get-all-projects.ts
+++ apps/coffee-board/src/actions/projects/get-all-projects.ts
@@ -1,15 +1,16 @@
 "use server"
 
+import { Project } from "@/entities/project.entity"
 import { pgQuery } from "@/lib/pg"
 
 const query = `
     SELECT * FROM v_cf_projects_base
 `
 
-export async function getAllProjects() {
+export async function getAllProjects(): Promise<Project[]> {
 	try {
 		const data = await pgQuery(query)
-		return data
+		return data as Project[]
 	} catch (error) {
 		console.error(error)
 		return []
