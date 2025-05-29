a5aa3bea85d20af6bac0db3eb535f968141020a6
Carlos Estrada <carabesle@gmail.com>
2025-05-28 19:54:22 -0600

    feat: add display of projects

--- apps/coffee-board/src/app/(core)/(sidebar-sections)/projects/_components/project-card.tsx
+++ apps/coffee-board/src/app/(core)/(sidebar-sections)/projects/_components/project-card.tsx
@@ -9,24 +9,36 @@ import {
 import { useRouter } from "next/navigation"
 import React from "react"
 import ProjectActions from "./project-actions"
+import { Project } from "@/entities/project.entity"
 
-export default function ProjectCard() {
+const ProjectCard = React.memo(function ProjectCard({
+	project,
+}: {
+	project: Project
+}) {
 	const router = useRouter()
 
 	return (
 		<Card>
 			<CardHeader>
 				<CardTitle className="flex items-center justify-between">
-					<div>Project 1</div>
+					<div>
+						{project.id} - {project.name}
+					</div>
 					<ProjectActions />
 				</CardTitle>
-				<CardDescription>Description</CardDescription>
+				<CardDescription>{project.description}</CardDescription>
 			</CardHeader>
 			<CardFooter>
-				<Button variant={"secondary"} onClick={() => router.push("/boards/1")}>
+				<Button
+					variant={"secondary"}
+					onClick={() => router.push(`/boards/${project.id}`)}
+				>
 					Go to project
 				</Button>
 			</CardFooter>
 		</Card>
 	)
-}
+})
+
+export default ProjectCard
---
+++ apps/coffee-board/src/app/(core)/(sidebar-sections)/projects/_components/project-container.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import ProjectCard from "./project-card"
+import { useProjects } from "../_hook/use-projects"
+import Icons from "@/components/shared/icons"
+
+const NoProjects = () => {
+	return (
+		<div className="flex items-center justify-center h-full p-4 flex-col gap-2 text-slate-500 dark:text-neutral-500">
+			<Icons.Misc.Books className="size-32" />
+			<p className="text-gray-500">
+				No projects found. Create a new project to get started.
+			</p>
+		</div>
+	)
+}
+
+export default function ProjectContainer() {
+	const { QProject, projects } = useProjects()
+
+	const isLoading =
+		QProject.isLoading || QProject.isFetching || QProject.isRefetching
+
+	return (
+		<div className="flex-1 overflow-y-auto bg-slate-100 rounded-lg dark:bg-neutral-900">
+			{isLoading && (
+				<div className="flex items-center justify-center h-full p-4 flex-col gap-2">
+					<Icons.Misc.Refresh className="size-32 animate-spin text-slate-500 dark:text-neutral-500" />
+					<p>Loading projects...</p>
+				</div>
+			)}
+			{!isLoading && projects.length === 0 && <NoProjects />}
+			{!isLoading && projects.length > 0 && (
+				<div className="gap-2 overflow-y-auto p-2 grid grid-cols-3">
+					{projects.map(project => (
+						<ProjectCard key={project.id} project={project} />
+					))}
+				</div>
+			)}
+		</div>
+	)
+}
