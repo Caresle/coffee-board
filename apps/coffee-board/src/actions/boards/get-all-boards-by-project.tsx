951e6247ecf7410286b176991a868689aeb689ff
Carlos Estrada <carabesle@gmail.com>
2025-05-30 21:06:46 -0600

    feat: add get all boards by project action

---
+++ apps/coffee-board/src/actions/boards/get-all-boards-by-project.tsx
@@ -0,0 +1,23 @@
+"use server"
+
+import { Board } from "@/entities/board.entity"
+import { pgQuery } from "@/lib/pg"
+
+const query = `
+    SELECT
+        *
+    FROM v_cf_boards
+    WHERE id_project = $1
+`
+
+export default async function getAllBoardsByProject(
+	projectId: number,
+): Promise<Board[]> {
+	try {
+		const boards = await pgQuery(query, [projectId])
+		return boards as Board[]
+	} catch (error) {
+		console.error(error)
+		return []
+	}
+}
