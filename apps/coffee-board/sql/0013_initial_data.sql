9f6ec7ec131a6f5c22559f745b70f77be83c1a8a
Carlos Estrada <carabesle@gmail.com>
2025-05-31 10:13:43 -0600

    feat(database): add initial info for tasks

--- apps/coffee-board/sql/0013_initial_data.sql
+++ apps/coffee-board/sql/0013_initial_data.sql
@@ -60,3 +60,70 @@ values
 	(6, 2, 'todo', 1, 0);
 
 select setval('board_details_id_seq', 6);
+
+-- Tasks
+insert into tasks (
+	id, id_board_det, name,
+	description, date_begin, date_end,
+	id_priority , id_assigned, time_estimation
+)
+values
+(
+	1, 1, 'Workouts section',
+	'Task related with the workouts module', null, null,
+	null, null, null
+),
+(
+	2, 1, 'Workout with priority',
+	'Description of the task', null, null,
+	1, null, null
+),
+(
+	3, 1, 'Workout with priority 2',
+	null, null, null,
+	2, null, null
+),
+(
+	4, 1, 'Workout with priority 3',
+	null, null, null,
+	3, null, null
+),
+(
+	5, 1, 'Workout with priority 3',
+	null, null, null,
+	4, null, null
+);
+
+
+select setval('tasks_id_seq', 4);
+
+-- task tags
+insert into task_tags (id, id_tag, id_task)
+values (1, 1, 1), (2, 1, 2), (3, 2, 1), (4, 3, 2);
+
+select setval('task_tags_id_seq', 4);
+
+-- task checklists
+insert into task_checklist (id, id_task, name)
+values (1, 3, 'Workflow done'), (2, 4, 'Requirements');
+
+select setval('task_checklist_id_seq', 2);
+
+insert into task_checklist_det (id, id_checklist, name, completed, level, id_parent)
+values
+	(1, 1, 'Task 1', 0, 1, null),
+	(2, 1, 'Task 2', 0, 1, null),
+	(3, 1, 'Task 2 - 1', 0, 2, 2),
+	(4, 1, 'Task 2 - 2', 1, 2, 2),
+	(5, 1, 'Task 3', 0, 1, null);
+
+select setval('task_checklist_det_id_seq', 5);
+
+insert into task_history(id, id_task, msg, stamp, id_user)
+values (1, 1, 'Message 1', '2025-05-31 08:53:00', 1),
+	(2, 1, 'Message 2', '2025-05-31 08:54:00', 1),
+	(3, 1, 'Message 3', '2025-05-31 09:53:00', 2),
+	(4, 1, 'Message 4', '2025-05-31 10:53:00', 1);
+
+
+select setval('task_history_id_seq', 4);
