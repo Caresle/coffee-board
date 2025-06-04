6af144b6128a1a00f7e16b7f635c7ab215a39d24
Carlos Estrada <carabesle@gmail.com>
2025-06-03 20:22:01 -0600

    feat: add queries for task section

--- apps/coffee-board/src/app/api/tasks/queries.ts
+++ apps/coffee-board/src/app/api/tasks/queries.ts
@@ -4,4 +4,21 @@ export const QueriesTask = {
         values ($1, $2, $3, $4, $5, $6, $7, $8)
         returning *
     `,
+	updateTask: `
+        update tasks
+        set name = $2,
+            description = $3,
+            date_begin = $4,
+            date_end = $5,
+            id_priority = $6,
+            id_assigned = $7,
+            time_estimation = $8
+        where id = $1
+        returning *
+    `,
+	deleteTask: `
+        delete from tasks
+        where id = $1
+        returning *
+    `,
 }
