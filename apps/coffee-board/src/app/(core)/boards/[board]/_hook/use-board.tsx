b057a43b9e08f37fa5ffc522faa0d3a7e557f895
Carlos Estrada <carabesle@gmail.com>
2025-06-07 10:26:17 -0600

    feat(task): add query fetching in use board hook

--- apps/coffee-board/src/app/(core)/boards/[board]/_hook/use-board.tsx
+++ apps/coffee-board/src/app/(core)/boards/[board]/_hook/use-board.tsx
@@ -1,14 +1,20 @@
 "use client"
 
+import { queryKeys } from "@/constants/queryKeys"
 import { BoardDetails } from "@/entities/board.entity"
+import { Task } from "@/entities/task.entity"
+import taskService from "@/services/task.service"
+import { useQuery, UseQueryResult } from "@tanstack/react-query"
 import { createContext, useContext } from "react"
 
 interface BoardContextType {
 	boardDetail: BoardDetails
+	QTasks: UseQueryResult<Task[]>
 }
 
 const BoardContext = createContext<BoardContextType>({
 	boardDetail: {} as BoardDetails,
+	QTasks: {} as UseQueryResult<Task[]>,
 })
 
 export const useBoard = () => useContext(BoardContext)
@@ -20,8 +26,19 @@ export function BoardDetailProvider({
 	boardDetail: BoardDetails
 	children: React.ReactNode
 }) {
+	const QTasks = useQuery({
+		queryKey: [
+			queryKeys.tasks,
+			{
+				id: boardDetail.id,
+			},
+		],
+		queryFn: () => taskService.getByBoardDetId(boardDetail.id),
+	})
+
 	const value: BoardContextType = {
 		boardDetail,
+		QTasks,
 	}
 
 	return <BoardContext.Provider value={value}>{children}</BoardContext.Provider>
