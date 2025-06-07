177fff3f98424333ee0c20a85d73864fdda8add9
Carlos Estrada <carabesle@gmail.com>
2025-06-07 16:34:47 -0600

    feat(task): add state for handle new tasks

--- apps/coffee-board/src/app/(core)/boards/[board]/_hook/use-board.tsx
+++ apps/coffee-board/src/app/(core)/boards/[board]/_hook/use-board.tsx
@@ -5,16 +5,22 @@ import { BoardDetails } from "@/entities/board.entity"
 import { Task } from "@/entities/task.entity"
 import taskService from "@/services/task.service"
 import { useQuery, UseQueryResult } from "@tanstack/react-query"
-import { createContext, useContext } from "react"
+import { createContext, useContext, useMemo, useState } from "react"
 
 interface BoardContextType {
 	boardDetail: BoardDetails
 	QTasks: UseQueryResult<Task[]>
+	tasks: Task[]
+	isNewTask: boolean
+	setIsNewTask: (isNewTask: boolean) => void
 }
 
 const BoardContext = createContext<BoardContextType>({
 	boardDetail: {} as BoardDetails,
 	QTasks: {} as UseQueryResult<Task[]>,
+	tasks: [],
+	isNewTask: false,
+	setIsNewTask: () => {},
 })
 
 export const useBoard = () => useContext(BoardContext)
@@ -26,6 +32,8 @@ export function BoardDetailProvider({
 	boardDetail: BoardDetails
 	children: React.ReactNode
 }) {
+	const [isNewTask, setIsNewTask] = useState(false)
+
 	const QTasks = useQuery({
 		queryKey: [
 			queryKeys.tasks,
@@ -36,9 +44,14 @@ export function BoardDetailProvider({
 		queryFn: () => taskService.getByBoardDetId(boardDetail.id),
 	})
 
+	const tasks = useMemo(() => QTasks?.data || [], [QTasks])
+
 	const value: BoardContextType = {
 		boardDetail,
 		QTasks,
+		tasks,
+		isNewTask,
+		setIsNewTask,
 	}
 
 	return <BoardContext.Provider value={value}>{children}</BoardContext.Provider>
