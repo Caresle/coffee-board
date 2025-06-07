df8be548ceb01d4db953fdc0ad01781579a2fbc6
Carlos Estrada <carabesle@gmail.com>
2025-06-07 10:24:52 -0600

    feat: add task entity

---
+++ apps/coffee-board/src/entities/task.entity.ts
@@ -0,0 +1,19 @@
+import { Priority } from "./priority.entity"
+import { Tag } from "./tag.entity"
+
+export interface Task {
+	id: number
+	id_board_det: number
+	name: string
+	decription: string | null
+	date_begin: Date | null
+	date_end: Date | null
+	id_priority: number | null
+	id_assigned: number | null
+	time_estimation: number | null
+	created_at: Date
+	priority: Priority
+	tags: Tag[]
+	checklist: []
+	history: []
+}
