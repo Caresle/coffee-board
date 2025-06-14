e045fd8b009e162b1d28fae1259b3d3f994c7402
Carlos Estrada <carabesle@gmail.com>
2025-06-13 20:14:16 -0600

    feat: add hook for handle render of checklist

--- apps/coffee-board/src/app/(core)/boards/[board]/_hook/use-checklist.tsx
+++ apps/coffee-board/src/app/(core)/boards/[board]/_hook/use-checklist.tsx
@@ -1,24 +1,35 @@
 "use client"
 
+import getChecklistByTask from "@/actions/tasks/checklist/get-checklist-by-task"
+import { queryKeys } from "@/constants/queryKeys"
 import {
 	TaskCheckList,
 	TaskCheckListDetails,
 	TaskCheckListHeader,
 } from "@/entities/task.entity"
-import { createContext, useContext } from "react"
+import { useQuery, UseQueryResult } from "@tanstack/react-query"
+import { createContext, useContext, useMemo } from "react"
 
 interface ChecklistTaskContext {
+	QSubChecklist: UseQueryResult<TaskCheckList>
 	checklist: TaskCheckList
 	header: TaskCheckListHeader
 	details: Array<TaskCheckListDetails>
 }
 
 const ChecklistTaskContext = createContext<ChecklistTaskContext>({
+	QSubChecklist: {} as UseQueryResult<TaskCheckList>,
 	checklist: {} as TaskCheckList,
 	header: {} as TaskCheckListHeader,
 	details: [] as Array<TaskCheckListDetails>,
 })
 
+/**
+ * Context used to provide the specific checklist of the task,
+ * for example if the task contains multiple checklist, this context
+ * will provide information related with a single checklist.
+ *
+ */
 export function useChecklistTaskContext() {
 	return useContext(ChecklistTaskContext)
 }
@@ -30,10 +41,31 @@ export function CheckListTaskProvider({
 	children: React.ReactNode
 	checklist: TaskCheckList
 }) {
+	/**
+	 * `QSubChecklist` is used to fetch the checklist of the task.
+	 * don't confuse with the global section of the checklist
+	 */
+	const QSubChecklist = useQuery({
+		queryKey: [
+			queryKeys.tasksChecklist,
+			{
+				id: checklist.header.id_task,
+			},
+		],
+		queryFn: () => getChecklistByTask(checklist.header.id_task),
+		initialData: checklist,
+	})
+
+	const { header, details } = useMemo(
+		() => QSubChecklist?.data || {},
+		[QSubChecklist],
+	)
+
 	const value: ChecklistTaskContext = {
 		checklist,
-		header: checklist.header || {},
-		details: checklist.details || [],
+		QSubChecklist,
+		header: header || {},
+		details: details || [],
 	}
 
 	return <ChecklistTaskContext value={value}>{children}</ChecklistTaskContext>
