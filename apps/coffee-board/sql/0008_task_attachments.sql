156b1c11a11c436b5aa8720d7fec7f54b5573d1f
Carlos Estrada <carabesle@gmail.com>
2025-06-17 20:03:29 -0600

    feat: add file_path in task_attachments

--- apps/coffee-board/sql/0008_task_attachments.sql
+++ apps/coffee-board/sql/0008_task_attachments.sql
@@ -3,6 +3,7 @@ create table task_attachments (
 	id_task int not null,
 	name varchar(255) not null,
 	file_type varchar(20) not null,
+	file_path varchar(255) not null,
 	created_at timestamp default now() not null,
 	foreign key (id_task)
 		references tasks(id)
