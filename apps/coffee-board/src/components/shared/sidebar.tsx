48b0ee8c6faa0ebf4972ee9d827da2ee814a9d66
Carlos Estrada <carabesle@gmail.com>
2025-06-21 09:13:08 -0600

    feat: add sidebar collapse

--- apps/coffee-board/src/components/shared/sidebar.tsx
+++ apps/coffee-board/src/components/shared/sidebar.tsx
@@ -1,15 +1,18 @@
 "use client"
-import React from "react"
+import React, { useState } from "react"
 import { cn } from "@/lib/utils"
 import { useRouter, usePathname } from "next/navigation"
 import { ROUTES } from "@/constants/routes"
 import ProjectTree from "./project-tree/project-tree"
+import { Button } from "../ui/button"
+import Icons from "./icons"
 
 type SidebarItemProps = {
 	icon: React.ElementType
 	isActive?: boolean
 	children: React.ReactNode
 	href?: string
+	isExpanded?: boolean
 }
 
 const SidebarItem = ({
@@ -17,6 +20,7 @@ const SidebarItem = ({
 	children,
 	isActive,
 	href,
+	isExpanded = true,
 }: SidebarItemProps) => {
 	const router = useRouter()
 
@@ -31,18 +35,19 @@ const SidebarItem = ({
 				"flex items-center gap-2 text-lg font-semibold p-2 rounded-lg transition-all hover:shadow-lg border border-transparent hover:border-blue-500 hover:scale-100 scale-95 cursor-pointer select-none hover:text-white hover:bg-blue-500",
 				{
 					"bg-blue-50 text-blue-500": isActive,
+					"w-fit": !isExpanded,
 				},
 			)}
 		>
 			<Icon className="size-5" />
-			{children}
+			{isExpanded && children}
 		</li>
 	)
 }
 
 export default function Sidebar() {
 	const pathname = usePathname()
-
+	const [isExpanded, setIsExpanded] = useState(true)
 	const activeRoute = ROUTES.filter(route => {
 		const currentRoute = pathname.split("/")?.[1]
 		const routePath = route.path?.split("/")?.[1]
@@ -52,21 +57,50 @@ export default function Sidebar() {
 		return currentRoute.includes(routePath)
 	})?.[0]
 
+	const ExpandedButton = () => {
+		return (
+			<>
+				<Icons.Actions.FirstPage className="size-5" />
+				Coffee Board
+			</>
+		)
+	}
+
 	return (
-		<div className="bg-white border rounded-lg w-1/5 p-1 dark:bg-neutral-800">
-			<ul className="flex flex-col gap-1">
+		<div
+			className={cn(
+				"bg-white border rounded-lg w-1/5 p-1 dark:bg-neutral-800",
+				{
+					"w-fit": !isExpanded,
+				},
+			)}
+		>
+			<Button
+				variant={"ghost"}
+				size={isExpanded ? "default" : "icon"}
+				onClick={() => setIsExpanded(!isExpanded)}
+				className={cn("w-full flex justify-start items-center", {
+					"justify-center items-center": !isExpanded,
+				})}
+			>
+				{isExpanded && <ExpandedButton />}
+
+				{!isExpanded && <Icons.Actions.LastPage className="size-5" />}
+			</Button>
+			<ul className={cn("flex flex-col gap-1", { "w-fit": !isExpanded })}>
 				{ROUTES.map((route, index) => (
 					<SidebarItem
 						icon={route.icon}
 						href={route.path}
 						key={index}
 						isActive={activeRoute?.path === route.path}
+						isExpanded={isExpanded}
 					>
 						{route.name}
 					</SidebarItem>
 				))}
 			</ul>
-			<ProjectTree />
+			{isExpanded && <ProjectTree />}
 		</div>
 	)
 }
