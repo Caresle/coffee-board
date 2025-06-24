488e325414e0a1f1855601d595caf902a0fa33c7
Carlos Estrada <carabesle@gmail.com>
2025-06-23 20:23:25 -0600

    feat: add board archive modal

---
+++ apps/coffee-board/src/app/(core)/boards/[board]/_components/_modals/_board/archive-board-modal.tsx
@@ -0,0 +1,72 @@
+import { Button } from "@/components/ui/button"
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog"
+import React from "react"
+import Icons from "@/components/shared/icons"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useDetailArchiveStore } from "../../../_states/boardDetails.state"
+import { BoardDetails } from "@/entities/board.entity"
+import { queryKeys } from "@/constants/queryKeys"
+import { useBoardGlobal } from "@/hooks/use-board-global"
+import boardDetailsService from "@/services/boardDetails.service"
+
+export default function ArchiveBoardModal() {
+	const { show, update, item } = useDetailArchiveStore(state => state)
+	const queryClient = useQueryClient()
+	const { selectedBoard } = useBoardGlobal()
+
+	const mut = useMutation({
+		mutationFn: boardDetailsService.archive,
+		onSuccess: () => {
+			update({ show: false, item: {} as BoardDetails })
+			queryClient.invalidateQueries({
+				queryKey: [queryKeys.boards, { id: selectedBoard?.id_project }],
+			})
+		},
+	})
+
+	const onSubmit = () => {
+		mut.mutate(item.id)
+	}
+
+	return (
+		<Dialog open={show} onOpenChange={value => update({ show: value })}>
+			<DialogContent className="max-w-none min-w-[35%]">
+				<DialogHeader>
+					<DialogTitle className="flex gap-2 items-center">
+						<Icons.Misc.Books />
+						Archive Board
+					</DialogTitle>
+					<DialogDescription>
+						This action will archive the board
+					</DialogDescription>
+				</DialogHeader>
+
+				<p>Are you sure you want to archive this board?</p>
+				<div className="bg-slate-100 p-2 rounded-lg border dark:bg-neutral-900 dark:border-neutral-800">
+					<div className="bg-white rounded-lg p-2 border dark:bg-neutral-800 dark:border-neutral-700">
+						{item.name}
+					</div>
+				</div>
+
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant={"secondary"} disabled={mut.isPending}>
+							Cancel
+						</Button>
+					</DialogClose>
+					<Button onClick={onSubmit} disabled={mut.isPending}>
+						Confirm
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
+}
--- apps/coffee-board/src/datasources/boardDetails.datasource.ts
+++ apps/coffee-board/src/datasources/boardDetails.datasource.ts
@@ -2,4 +2,6 @@ import { BoardDetails } from "@/entities/board.entity"
 import { GeneralDatasource } from "./general.datasource"
 
 export interface BoardDetailsDatasource
-	extends GeneralDatasource<BoardDetails> {}
+	extends GeneralDatasource<BoardDetails> {
+	archive(id: number): Promise<void>
+}
