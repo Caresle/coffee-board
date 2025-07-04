ed9edc82ba3546267db81251a1bfa4839e205cb2
Carlos Estrada <carabesle@gmail.com>
2025-07-03 19:20:08 -0600

    feat(sockets): create server on `apps/sockets-server` Fixes #176

--- apps/socket-server/src/index.ts
+++ apps/socket-server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello world ts socket server");
+import * as DotEnv from "dotenv";
+DotEnv.config();
+
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 4000;
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
