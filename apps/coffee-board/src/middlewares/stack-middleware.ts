ac2e52ac20a8292f02f6fffabee2be45b37befe4
Carlos Estrada <carabesle@gmail.com>
2025-07-07 19:06:12 -0600

    feat(auth): add middleware to protect endpoints according with the permission Fixes #257

---
+++ apps/coffee-board/src/middlewares/stack-middleware.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+import { NextResponse } from "next/server"
+
+export const stackMiddleware = (functions: Function[] = [], index = 0) => {
+	const current = functions[index]
+
+	if (current) {
+		const next: any = stackMiddleware(functions, index + 1)
+		if (typeof current === "function") {
+			const fn = current as Function
+			return fn(next)
+		}
+	}
+
+	return () => NextResponse.next()
+}
